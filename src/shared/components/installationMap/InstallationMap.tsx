d46889af5f7925707891bf110a3842c90cdd6d02
Robert Sheridan <sheridan@cbio.mskcc.org>
2018-10-11 12:15:25 -0400

    added installation map and images

--- src/shared/components/installationMap/InstallationMap.tsx
+++ src/shared/components/installationMap/InstallationMap.tsx
@@ -24,11 +24,12 @@ export default class InstallationMap extends React.Component <{}, {}> {
 
 
     public render() {
+        let installationMapImgSrc = require("./images/installationMapFingerprint.png");
         return (
             <div className={styles.installationMap}>
                 <div className="installationMap-links">
                     <p></p>
-                    <a href="https://fusiontables.google.com/data?docid=1mudBCbNaPYLZH9UUcgpt4gT5b0Sp3o8rzZDDxIHf&pli=1#map:id=3">View Map</a>
+                    <iframe width="300" height="300" scrolling="no" frameborder="no" src="https://fusiontables.google.com/embedviz?q=select+col5+from+1mudBCbNaPYLZH9UUcgpt4gT5b0Sp3o8rzZDDxIHf&amp;viz=MAP&amp;h=false&amp;lat=28.673681979724474&amp;lng=-72.84259781562483&amp;t=1&amp;z=1&amp;l=col5&amp;y=2&amp;tmplt=2&amp;hml=ONE_COL_LAT_LNG"></iframe>
                     <a href="https://docs.google.com/forms/d/e/1FAIpQLSflQdN956q7Xh5caO8z8jIaF6uMLBkKrSxFvPi8OhNBWB247w/viewform">Add your portal to the map</a>
                 </div>
             </div>
